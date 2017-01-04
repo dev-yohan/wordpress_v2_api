ead5392f4638e0efc7ea13e17b69d1162eb1bd0e
Yohan Morales <yohanmorales@hotmail.com>
2017-01-04 16:53:26 -0300

    :wrench: simplecov configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'codeclimate-test-reporter'
 require 'simplecov'
 require_relative '../lib/wp_api'
 
+SimpleCov.start
+
 ENV['HTTP_PROXY'] = 'http://10.28.2.251:3128'
 config = WpApi::Configuration.new
 config.base_uri = 'http://live-asuntos-de-mujeres.pantheonsite.io'
